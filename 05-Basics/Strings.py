c09045031ac47897fee3f35152b57009bd637165
Albert <albert.kuc@gmail.com>
2018-06-30 15:10:08 +0100

    Created new file for basic operations on strings and variables
    Based on Ch5 lesson 26: More About Variables and Strings. Contains exmples of some basic operations on strings.

---
+++ 05-Basics/Strings.py
@@ -0,0 +1,23 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+print(parrot[0])
+print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "6,528,821,587,657,222,374"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+
+print("Hello " *5)
+today = "Saturday"
+print("day" in today)
+print("fri" in today)
+print("parrot" in "fjord")
